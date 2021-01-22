362b75774482cc0d431902fe33f2132193770eac
Anand Mahesh <manand881@gmail.com>
2021-01-22 11:01:00 +0530

    init

---
+++ Readme.md
@@ -0,0 +1,1 @@
+#   Brigade Point Cloud 
---
+++ pointclouds/brigade/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 47751592,
+    "boundingBox": {
+        "lx": 8633401.343922047,
+        "ly": 1460792.846379842,
+        "lz": 746.9124686820877,
+        "ux": 8633882.221922048,
+        "uy": 1461273.7243798425,
+        "uz": 1227.7904686825796
+    },
+    "tightBoundingBox": {
+        "lx": 8633401.343922047,
+        "ly": 1460792.846379842,
+        "lz": 746.9124686820877,
+        "ux": 8633882.221922048,
+        "uy": 1461193.565379842,
+        "uz": 959.9924686820876
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 4.164525508880615,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
